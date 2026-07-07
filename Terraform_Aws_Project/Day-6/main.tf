858faa794100e9ded4be4214a153d415dde440d6
ashisnishanka <43777120+ashisnishanka@users.noreply.github.com>
2026-07-07 10:00:26 +0530

    Update main.tf

--- Terraform_Aws_Project/Day-6/main.tf
+++ Terraform_Aws_Project/Day-6/main.tf
@@ -1,28 +1,4 @@
 
-
-# # it will create s3 bucket in aws 
-# resource "aws_s3_bucket" "idream-bk" {
-# bucket = "idreamit-bucket-123456"
-# #bucket = local.bucket_name
-#  tags = {
-#   #Name = "dev-bucket-idream"
-   
-#    Environment =var.environment
-#    #Environment = var.environment
-#  }
-# }
-# resource "aws_vpc" "myvpc" {
-# cidr_block = "10.0.1.0/24"
-# #region = "ap-south-1"
-# region = var.region
-# tags = {
-# Environment=var.environment
-# #Name = local.vpc_name
-
-# }
-
-# }
-
 # it will create s3 bucket in aws 
 resource "aws_s3_bucket" "idream-bk" {
 #bucket = "idreamit-bucket-123456"
